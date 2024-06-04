a7222b85008c50d5a6432768ba8945011efd8026
ataide <scorpionspfc@gmail.com>
2024-06-03 18:29:44 -0600

    test

---
+++ src/app/uses/page.jsx
@@ -0,0 +1,104 @@
+export default function Uses() {
+    return (
+        <main className="container mx-auto p-4 flex flex-col lg:flex-row lg:space-x-4">
+        <div class="container mx-auto">
+            <div>
+                <h1 class="text-4xl font-bold mb-4">Software I use, Tools, and other things</h1>
+                <p class="mb-10 md:w w-2/4"> Lorem, ipsum dolor sit amet consectetur adipisicing elit. Eveniet voluptatum vel iure, blanditiis esse quasi ex temporibus cumque asperiores ab praesentium numquam nostrum incidunt? Eveniet aliquid animi deleniti voluptatibus! Sunt!</p>
+            </div>
+        <div class="flex rounded-sm shadow-sm mt-20">
+            <div class="w-1/4 ">
+                <h2 class="text-lg font-semibold">Workstation</h2>
+            </div>
+            <div class="w-3/4">
+                <div>
+                    <h3 className="text-xl font-bold">Item Name</h3>
+                    <p className="mt-2">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut et massa mi. Aliquam in hendrerit urna. Pellentesque sit amet sapien fringilla, mattis ligula consectetur, ultrices mauris. Maecenas vitae mattis tellus. Nullam quis imperdiet augue. Vestibulum auctor ornare leo, non suscipit magna interdum eu.</p>
+                </div>
+                <div className="mt-6">
+                    <h3 className="text-xl font-bold">Item Name</h3>
+                    <p className="mt-2">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut et massa mi. Aliquam in hendrerit urna. Pellentesque sit amet sapien fringilla, mattis ligula consectetur, ultrices mauris. Maecenas vitae mattis tellus. Nullam quis imperdiet augue. Vestibulum auctor ornare leo, non suscipit magna interdum eu.</p>
+                </div>
+                <div className="mt-6">
+                    <h3 className="text-xl font-bold">Item Name</h3>
+                    <p className="mt-2">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut et massa mi. Aliquam in hendrerit urna. Pellentesque sit amet sapien fringilla, mattis ligula consectetur, ultrices mauris. Maecenas vitae mattis tellus. Nullam quis imperdiet augue. Vestibulum auctor ornare leo, non suscipit magna interdum eu.</p>
+                </div>
+                <div className="mt-6">
+                    <h3 className="text-xl font-bold">Item Name</h3>
+                    <p className="mt-2">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut et massa mi. Aliquam in hendrerit urna. Pellentesque sit amet sapien fringilla, mattis ligula consectetur, ultrices mauris. Maecenas vitae mattis tellus. Nullam quis imperdiet augue. Vestibulum auctor ornare leo, non suscipit magna interdum eu.</p>
+                </div>
+            </div>
+        </div>
+        <div class="flex rounded-sm shadow-sm mt-20">
+            <div class="w-1/4 ">
+                <h2 class="text-lg font-semibold">Dev Tools</h2>
+            </div>
+            <div class="w-3/4">
+                <div>
+                    <h3 className="text-xl font-bold">Item Name</h3>
+                    <p className="mt-2">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut et massa mi. Aliquam in hendrerit urna. Pellentesque sit amet sapien fringilla, mattis ligula consectetur, ultrices mauris. Maecenas vitae mattis tellus. Nullam quis imperdiet augue. Vestibulum auctor ornare leo, non suscipit magna interdum eu.</p>
+                </div>
+                <div className="mt-6">
+                    <h3 className="text-xl font-bold">Item Name</h3>
+                    <p className="mt-2">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut et massa mi. Aliquam in hendrerit urna. Pellentesque sit amet sapien fringilla, mattis ligula consectetur, ultrices mauris. Maecenas vitae mattis tellus. Nullam quis imperdiet augue. Vestibulum auctor ornare leo, non suscipit magna interdum eu.</p>
+                </div>
+                <div className="mt-6">
+                    <h3 className="text-xl font-bold">Item Name</h3>
+                    <p className="mt-2">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut et massa mi. Aliquam in hendrerit urna. Pellentesque sit amet sapien fringilla, mattis ligula consectetur, ultrices mauris. Maecenas vitae mattis tellus. Nullam quis imperdiet augue. Vestibulum auctor ornare leo, non suscipit magna interdum eu.</p>
+                </div>
+                <div className="mt-6">
+                    <h3 className="text-xl font-bold">Item Name</h3>
+                    <p className="mt-2">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut et massa mi. Aliquam in hendrerit urna. Pellentesque sit amet sapien fringilla, mattis ligula consectetur, ultrices mauris. Maecenas vitae mattis tellus. Nullam quis imperdiet augue. Vestibulum auctor ornare leo, non suscipit magna interdum eu.</p>
+                </div>
+            </div>
+        </div>
+        <div class="flex rounded-sm shadow-sm mt-20">
+            <div class="w-1/4 ">
+                <h2 class="text-lg font-semibold">Design</h2>
+            </div>
+            <div class="w-3/4">
+                <div>
+                    <h3 className="text-xl font-bold">Item Name</h3>
+                    <p className="mt-2">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut et massa mi. Aliquam in hendrerit urna. Pellentesque sit amet sapien fringilla, mattis ligula consectetur, ultrices mauris. Maecenas vitae mattis tellus. Nullam quis imperdiet augue. Vestibulum auctor ornare leo, non suscipit magna interdum eu.</p>
+                </div>
+                <div className="mt-6">
+                    <h3 className="text-xl font-bold">Item Name</h3>
+                    <p className="mt-2">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut et massa mi. Aliquam in hendrerit urna. Pellentesque sit amet sapien fringilla, mattis ligula consectetur, ultrices mauris. Maecenas vitae mattis tellus. Nullam quis imperdiet augue. Vestibulum auctor ornare leo, non suscipit magna interdum eu.</p>
+                </div>
+                <div className="mt-6">
+                    <h3 className="text-xl font-bold">Item Name</h3>
+                    <p className="mt-2">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut et massa mi. Aliquam in hendrerit urna. Pellentesque sit amet sapien fringilla, mattis ligula consectetur, ultrices mauris. Maecenas vitae mattis tellus. Nullam quis imperdiet augue. Vestibulum auctor ornare leo, non suscipit magna interdum eu.</p>
+                </div>
+                <div className="mt-6">
+                    <h3 className="text-xl font-bold">Item Name</h3>
+                    <p className="mt-2">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut et massa mi. Aliquam in hendrerit urna. Pellentesque sit amet sapien fringilla, mattis ligula consectetur, ultrices mauris. Maecenas vitae mattis tellus. Nullam quis imperdiet augue. Vestibulum auctor ornare leo, non suscipit magna interdum eu.</p>
+                </div>
+            </div>
+        </div>
+        <div class="flex rounded-sm shadow-sm mt-20">
+            <div class="w-1/4 ">
+                <h2 class="text-lg font-semibold">Productivity</h2>
+            </div>
+            <div class="w-3/4">
+                <div>
+                    <h3 className="text-xl font-bold">Item Name</h3>
+                    <p className="mt-2">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut et massa mi. Aliquam in hendrerit urna. Pellentesque sit amet sapien fringilla, mattis ligula consectetur, ultrices mauris. Maecenas vitae mattis tellus. Nullam quis imperdiet augue. Vestibulum auctor ornare leo, non suscipit magna interdum eu.</p>
+                </div>
+                <div className="mt-6">
+                    <h3 className="text-xl font-bold">Item Name</h3>
+                    <p className="mt-2">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut et massa mi. Aliquam in hendrerit urna. Pellentesque sit amet sapien fringilla, mattis ligula consectetur, ultrices mauris. Maecenas vitae mattis tellus. Nullam quis imperdiet augue. Vestibulum auctor ornare leo, non suscipit magna interdum eu.</p>
+                </div>
+                <div className="mt-6">
+                    <h3 className="text-xl font-bold">Item Name</h3>
+                    <p className="mt-2">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut et massa mi. Aliquam in hendrerit urna. Pellentesque sit amet sapien fringilla, mattis ligula consectetur, ultrices mauris. Maecenas vitae mattis tellus. Nullam quis imperdiet augue. Vestibulum auctor ornare leo, non suscipit magna interdum eu.</p>
+                </div>
+                <div className="mt-6">
+                    <h3 className="text-xl font-bold">Item Name</h3>
+                    <p className="mt-2">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut et massa mi. Aliquam in hendrerit urna. Pellentesque sit amet sapien fringilla, mattis ligula consectetur, ultrices mauris. Maecenas vitae mattis tellus. Nullam quis imperdiet augue. Vestibulum auctor ornare leo, non suscipit magna interdum eu.</p>
+                </div>
+            </div>
+        </div>
+    </div>
+    </main>
+    )
+};
